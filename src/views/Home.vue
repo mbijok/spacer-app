472792874bac0b8c46b0b750d3bdf1cdb5fd1c83
Mariusz Bijok <mbijok@outlook.com>
2021-10-11 12:59:30 +0200

    Init search component

--- src/views/Home.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="home">
-    <h1>spacer</h1>
-  </div>
-</template>
-
-<script setup></script>
---
+++ src/views/Search.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="search flex align-center text-center flex-col">
+      <label for="search">Search</label>
+      <input
+        class="border-b-2 w-1/2 m-auto"
+        name="search"
+        v-model="searchValue"
+        @input="handleInput"
+      />
+      <p>{{ searchValue }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const searchValue = ref('');
+
+function handleInput() {
+  console.log(searchValue.value);
+}
+</script>
