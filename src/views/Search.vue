8aad7349077cca419fb55b94f73c9d5632ef2527
Mariusz Bijok <mbijok@outlook.com>
2021-10-13 13:51:13 +0200

    Connect to API

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -8,17 +8,34 @@
         v-model="searchValue"
         @input="handleInput"
       />
-      <p>{{ searchValue }}</p>
+
+      <ul>
+        <li v-for="item in results" :key="item.data[0].nasa_id">
+          <p>{{ item.data[0].title }}</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import axios from 'axios';
+import debounce from 'lodash.debounce';
+
+const api_url = 'https://images-api.nasa.gov/search';
+
+const results = ref([]);
 
 const searchValue = ref('');
-
-function handleInput() {
-  console.log(searchValue.value);
-}
+const handleInput = debounce(() => {
+  axios
+    .get(`${api_url}?q=${searchValue.value}&media_type=image`)
+    .then((response) => {
+      results.value = response.data.collection.items;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}, 500);
 </script>
