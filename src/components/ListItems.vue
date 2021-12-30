fa07b624f00f803b55f4bc56e5db727f0c38d0a3
Mariusz Bijok <mbijok@outlook.com>
2021-12-30 14:02:49 +0100

    Some grid styling stuff

--- src/components/ListItems.vue
+++ src/components/ListItems.vue
@@ -1,6 +1,36 @@
 <template>
-  <div :id="id" class="aspect-square border-4" :key="id">
-    <img :src="image" :alt="title" class="object-cover h-full w-full" />
+  <div
+    :id="id"
+    class="group aspect-square grid border-4 overflow-hidden cursor-pointer"
+    :key="id"
+  >
+    <img
+      :src="image"
+      :alt="title"
+      class="row-[1/2] col-[1/2] object-cover h-full w-full"
+    />
+    <p
+      class="
+        row-[1/2]
+        col-[1/2]
+        place-self-end
+        bg-gradient-to-t
+        from-black/75
+        via-black/75
+        text-white
+        font-semibold
+        pt-8
+        pb-2
+        px-2
+        h-min
+        w-full
+        translate-y-full
+        group-hover:translate-y-0
+        transition-transform
+      "
+    >
+      {{ title }}
+    </p>
   </div>
 </template>
 
@@ -14,6 +44,5 @@ const props = defineProps({
 });
 const id = props.item.data[0].nasa_id;
 const title = props.item.data[0].title;
-const description = props.item.data[0].description;
 const image = props.item.links[0].href;
 </script>
